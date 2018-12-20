1cf52b11bc4b8adb80c12643f445c1cd97409680
Tommy <tperez08483@gmail.com>
2018-12-19 19:07:13 -0600

    Clean up main.js

--- js/main.js
+++ js/main.js
@@ -1,32 +1,17 @@
-        // Sidenav
-        const sideNav = document.querySelector('.sidenav');
-        M.Sidenav.init(sideNav, {});
-        // Slider
-        const slider = document.querySelector('.slider');
-        M.Slider.init(slider, {
-          indicators: false,
-          height: 660,
-          transition: 500,
-          interval: 6000
-        });
-        // Autocomplete
-        const ac = document.querySelector('.autocomplete');
-        M.Autocomplete.init(ac, {
-          data: {
-            "Aruba": null,
-            "Cancun": null,
-            "Hawaii": null,
-            "Florida": null,
-            "California": null,
-            "Jamacia": null,
-            "Europe": null,
-            "Santorini Greece": null,
-            "Ixtapa": null
-          }
-        });
-        // Material Boxed
-        const mb = document.querySelectorAll('.materialboxed');
-        M.Materialbox.init(mb,{});
-        // ScrollSpy
-        const ss = document.querySelectorAll('.scrollspy');
-        M.ScrollSpy.init(ss, {});
+// Sidenav
+const sideNav = document.querySelector('.sidenav');
+M.Sidenav.init(sideNav, {});
+// Slider
+const slider = document.querySelector('.slider');
+M.Slider.init(slider, {
+  indicators: false,
+  height: 660,
+  transition: 500,
+  interval: 6000
+});
+// Material Boxed
+const mb = document.querySelectorAll('.materialboxed');
+M.Materialbox.init(mb,{});
+// ScrollSpy
+const ss = document.querySelectorAll('.scrollspy');
+M.ScrollSpy.init(ss, {});
